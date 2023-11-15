1faec08451983c8c40602bb6e2d2ca70fb97d079
Lucas Teixeira <lucas.lemos97@live>
2023-11-15 16:36:06 -0300

    projeto criado e aplicacao iniciada

---
+++ mscompras/src/test/java/com/teixeira/mscompras/MscomprasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teixeira.mscompras;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MscomprasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
