af47803cf421e0aacda9fa8a591728148f4a82fa
Lucas Teixeira <lucas.lemos97@live>
2024-01-09 15:22:03 -0300

    aula11

--- mscompras/src/main/java/com/teixeira/mscompras/service/rabbitmq/Producer.java
+++ mscompras/src/main/java/com/teixeira/mscompras/service/rabbitmq/Producer.java
@@ -1,7 +1,9 @@
 package com.teixeira.mscompras.service.rabbitmq;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.teixeira.mscompras.model.Pedido;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,9 +16,12 @@ public class Producer {
 
     private final RabbitTemplate rabbitTemplate;
     private final Queue queue;
+    private final ObjectMapper mapper;
 
+    @SneakyThrows
     @PostMapping
     public void enviarPedido(Pedido pedido){
         rabbitTemplate.convertAndSend(queue.getName(), pedido);
+        rabbitTemplate.convertAndSend(queue.getName(), mapper.writeValueAsString(pedido));
     }
 }
---
+++ workercompras/src/main/java/com/workercompras/model/Pedido.java
@@ -0,0 +1,53 @@
+package com.teixeira.mscompras.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.PropertyNamingStrategies;
+import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import lombok.*;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Builder
+@JsonNaming(PropertyNamingStrategies.SnakeCaseStrategy.class)
+@Entity(name = "tb_pedido")
+@ToString
+public class Pedido implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    private String nome;
+
+    @NotNull
+    @Min(1)
+    private Long produto;
+
+    @NotNull
+    @Min(1)
+    private BigDecimal valor;
+
+    @JsonFormat(pattern = "yyyy-mm-dd")
+    @NotNull
+    private Date dataCompra;
+
+    @NotBlank
+    private String cpfCliente;
+
+    @NotBlank
+    private String cep;
+}
